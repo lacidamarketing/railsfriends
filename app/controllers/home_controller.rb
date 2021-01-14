fe631ced7f223ea561262a59272bf8dd5c42f5ce
Ramer Lacida <dforthster@gmail.com>
2021-01-14 09:36:12 +0800

    initial commit

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,21 @@
+class HomeController < ApplicationController
+  def index
+  end
+
+
+
+
+
+
+
+
+
+
+
+
+  def about
+  	@about_me = "My name is Ramer and welcome to my ruby page"
+  end
+
+
+end
---
+++ db/migrate/20210113123020_add_user_id_friends.rb
@@ -0,0 +1,4 @@
+class AddUserIdFriends < ActiveRecord::Migration[6.1]
+  def change
+  end
+end
